bf17a16416ed13b13757a027109088e0d88dc4ff
Val Knight <val+git@valknight.xyz>
2020-01-05 20:41:02 +0000

    Define potential errors

---
+++ android_file_bridge/error.py
@@ -0,0 +1,11 @@
+class Error(Exception):
+    """Base class"""
+    pass
+
+class AdbConnectionError(Exception):
+    """Raised when ADB can't connect"""
+    pass
+
+class AdbPushError(Exception):
+    """Raised when a file can't be pushed"""
+    pass
